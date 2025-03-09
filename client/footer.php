208bbedbadb85772209e4bb8a0d63a86e4c304f5
SSZASeyam <118447914+sszobaer@users.noreply.github.com>
2025-03-09 22:40:55 +0600

    Dynamic footer added

---
+++ client/footer.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<footer class="footer-area">
+        <div class="container">
+            <div class="mainText">Code Craft Zobaer Limited</div>
+                <div class="social">
+                    <h5 class="text-follow">FOLLOW ME</h5>
+                    <a href="https://www.facebook.com/zobaer.ahmed.121" class="facebook"><i class="fab fa-facebook"></i></a>
+                    <a href="https://www.instagram.com/sheikh_shah_zobaer/" class="instagram"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.youtube.com/@CodeCraftZobaer" class="youtube"><i class="fab fa-youtube"></i></a>
+                    <a href="https://x.com/sszobaer"><i class="fab fa-twitter" class="twitter"></i></a>
+                    <a href="https://www.linkedin.com/in/s-s-zobaer-ahmed-209bab296/" class="linkedin"><i class="fab fa-linkedin"></i></a>
+                    <a href="https://github.com/sszobaer"><i class="fab fa-github" class="github"></i></a>
+                </div>
+                <div class="copyrights">
+                    <p class="para">
+                        Copyright ©<?php echo DATE("Y"); ?> All rights reserved | This is made with by
+                        <a href="#"><span style="color: var(--primary-color);">Code Craft Zobaer</span></a>
+                    </p>
+                </div>
+        </div>
+    </footer>
+
+</body>
+</html>
